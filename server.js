f4d5dc4eb9d3412cd5476fd4dc12a9d733acad32
Michael Blanton <mblanton@gmail.com>
2017-09-27 13:22:12 -0700

    More HBS tweaks

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+// Logger that logs requests
+app.use((req, resp, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server log');
+    }
+  });
+  next();
+});
+
+// Call response.render in middleware, but don't call next
+// app.use((req, resp, next) => {
+//   resp.render('maintenance.hbs', {
+//     pageTitle: 'Maintenance Page',
+//     welcomeMessage: "We'll be right back!"
+//   });
+// });
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
